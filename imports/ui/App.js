5afb683747d85929d71f5a66dd3535347fead7cf
Thomas Liu <tsmliu213@gmail.com>
2019-04-21 17:56:39 -0700

    feat: pull and display data from database and add meteor subscription events

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,57 +1,31 @@
 import React from "react";
+import { Meteor } from "meteor/meteor";
+import PropTypes from "prop-types";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import AppBar from "material-ui/AppBar";
 import { List } from "material-ui/List";
 import Divider from "material-ui/Divider";
+import { createContainer } from "meteor/react-meteor-data";
+import { Link } from "react-router-dom";
+
+// database
+import { Players } from "../api/players";
 
 import TeamList from "./Team-list";
 import TeamStats from "./Team-stats";
 import Player from "./Player";
 
 class App extends React.Component {
-  getPlayers = () => {
-    return [
-      {
-        _id: 1,
-        name: "John Leer",
-        ballManipulation: 1,
-        kickingAbilities: 4,
-        passingAbilities: 2,
-        duelTacking: 2,
-        fieldCoverage: 5,
-        blockingAbilities: 1,
-        gameStrategy: 3,
-        playmakingRisks: 2
-      },
-      {
-        _id: 2,
-        name: "Hola",
-        ballManipulation: 1,
-        kickingAbilities: 4,
-        passingAbilities: 2,
-        duelTacking: 2,
-        fieldCoverage: 5,
-        blockingAbilities: 1,
-        gameStrategy: 3,
-        playmakingRisks: 2
-      },
-      {
-        _id: 3,
-        name: "Manny",
-        ballManipulation: 1,
-        kickingAbilities: 4,
-        passingAbilities: 2,
-        duelTacking: 2,
-        fieldCoverage: 5,
-        blockingAbilities: 1,
-        gameStrategy: 3,
-        playmakingRisks: 2
-      }
-    ];
-  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      players: []
+    };
+  }
 
   renderPlayers = () => {
-    return this.getPlayers().map(player => (
+    return this.props.players.map(player => (
       <TeamList key={player._id} player={player} />
     ));
   };
@@ -70,6 +44,10 @@ class App extends React.Component {
               <Player />
             </div>
             <div className="col s12 m5">
+              <h2>Team list</h2>
+              <Link to="/new" className="waves-effect waves-light btn">
+                Add Player
+              </Link>
               <Divider />
               <List>{this.renderPlayers()}</List>
               <Divider />
@@ -84,4 +62,14 @@ class App extends React.Component {
   }
 }
 
-export default App;
+App.propTypes = {
+  players: PropTypes.array.isRequired
+};
+
+export default createContainer(() => {
+  Meteor.subscribe("players");
+
+  return {
+    players: Players.find({}, { sort: { name: 1 } }).fetch()
+  };
+}, App);
